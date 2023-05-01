783b48017392aead834ad96e813c84ce339b33d9
Dmitry Kabak <userdima@gmail.com>
2023-05-01 19:14:52 +0200

    Add mute button

--- Assets/Delivery/Behaviour/GameOverTracker.cs
+++ Assets/Delivery/Behaviour/GameOverTracker.cs
@@ -10,6 +10,7 @@ public class GameOverTracker : MonoBehaviour
     public GameObject gameOverCamera;
     public GameObject gameWonCamera;
     public TMP_Text scoreLabel;
+    public AudioSource sounds;
     public float transitionDelay = 5;
     private bool done;
 
@@ -31,6 +32,7 @@ public class GameOverTracker : MonoBehaviour
     IEnumerator GameOver()
     {
         Time.timeScale = 0;
+        sounds.enabled = false;
         yield return new WaitForSecondsRealtime(transitionDelay);
         Camera.main.gameObject.SetActive(false);
         gameOverCamera.SetActive(true);
@@ -42,6 +44,7 @@ public class GameOverTracker : MonoBehaviour
     {
         Time.timeScale = 0;
         yield return new WaitForSecondsRealtime(transitionDelay);
+        sounds.enabled = false;
         Camera.main.gameObject.SetActive(false);
         gameWonCamera.SetActive(true);
     }
